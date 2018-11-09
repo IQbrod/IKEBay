57649754fc2b2f0169122f196dd157bafb18d54c
Tuo018 <zhaotuo018@gmail.com>
2018-11-09 14:40:55 +0100

    ProductService + ProductMapper

--- src/main/java/ikb/service/ProductService.java
+++ src/main/java/ikb/service/ProductService.java
@@ -1,6 +1,8 @@
 package ikb.service;
 
 
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -8,6 +10,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Page;
 import org.springframework.transaction.annotation.Transactional;
 
+import ikb.product.Product;
+import ikb.repository.ProductRepository;
 import ikb.service.dto.ProductDTO;
 
 /**
@@ -19,17 +23,26 @@ public class ProductService {
 
     private final Logger log = LoggerFactory.getLogger(ProductService.class);
 
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository){
+        this.productRepository = productRepository;
+    }
+
     public void getAllProducts(){
 
     }
     public Page<ProductDTO> getAllManagedProducts(Pageable pageable){
         return null;
     }
-    public void getProduct(Long id){
+    public Optional<Product> getProduct(int id){
+
+        return productRepository.findOneById(id);
 
     }
     public void createProduct(String name, String description, String specification, String photolink, float price){
 
+        
     }
     //public void UpdateProduct();
     //public void DeleteProduct(); //note : should instead set a flag (named for example 'hidden') in the product), as we want to keep it in database
