710719339a38442a66c7053b4eec6f6b2b3689ed
yannou38 <yann.ducruy@live.fr>
2018-12-11 11:59:00 +0100

    paiement fonctionnel

--- src/main/webapp/app/paiement/components/infopaiement/infopaiement.component.ts
+++ src/main/webapp/app/paiement/components/infopaiement/infopaiement.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { PaiementInfos, PaymentType } from 'app/paiement/paiementinfo.model';
-import { debug } from 'util';
+import { Router } from '@angular/router';
+import { PaiementService } from 'app/paiement/paiement.service';
 
 @Component({
     selector: 'jhi-infopaiement',
@@ -11,7 +12,7 @@ import { debug } from 'util';
 export class InfoPaiementComponent implements OnInit {
     formdata: PaiementInfos;
 
-    constructor() {}
+    constructor(private paiementserv: PaiementService, private router: Router) {}
 
     ngOnInit() {
         this.formdata = new PaiementInfos(PaymentType.creditcard, '', '', '', '');
@@ -36,7 +37,6 @@ export class InfoPaiementComponent implements OnInit {
     validateExpiryDate(): boolean {
         // todo : tester le mois et l'année (mois : nombre /1000 <13;année : nombre%100>=annéeactuelle%100)
         if (this.validateExpiry()) {
-            console.log(new Date().getMonth());
             return (
                 parseInt(this.formdata.expiry.replace(/[/]+/g, ''), 10) % 100 > new Date().getFullYear() % 100 ||
                 (parseInt(this.formdata.expiry.replace(/[/]+/g, ''), 10) / 100 >= new Date().getMonth() + 1 &&
@@ -48,7 +48,8 @@ export class InfoPaiementComponent implements OnInit {
     }
 
     saveinfo(form: NgForm) {
-        console.log(this.formdata);
+        this.paiementserv.formobject = this.formdata;
+        this.router.navigate(['/paiementvalide']);
         return;
     }
 }
