a9b91e0f82efaac9131db6e8066841b40a062fe8
yannou38 <yann.ducruy@live.fr>
2018-12-11 17:20:28 +0100

    added vendor role, edited register form to add a checkbox

--- src/main/java/ikb/web/rest/AccountResource.java
+++ src/main/java/ikb/web/rest/AccountResource.java
@@ -61,7 +61,7 @@ public class AccountResource {
         if (!checkPasswordLength(managedUserVM.getPassword())) {
             throw new InvalidPasswordException();
         }
-        User user = userService.registerUser(managedUserVM, managedUserVM.getPassword());
+        User user = userService.registerUser(managedUserVM, managedUserVM.getPassword(), managedUserVM.isIsVendor());
         mailService.sendActivationEmail(user);
     }
 
