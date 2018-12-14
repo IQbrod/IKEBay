a0fc90d6c09ed1a3519a0bf1096c21f547cac396
IQbrod <iqbrod@outlook.fr>
2018-12-14 08:59:26 +0100

    SearchBar fonctionnelle /!\ Manque lien Searchbar - Filter

--- src/main/webapp/app/product/product.module.ts
+++ src/main/webapp/app/product/product.module.ts
@@ -7,10 +7,11 @@ import { ProductService } from 'app/product/product.service';
 import { ProductlistComponent } from './views/list/productlist.component';
 import { CommonModule } from '@angular/common';
 import { ProductRoute } from './product.route';
+import { ProductPipe } from 'app/product/pipes/product.pipe';
 
 @NgModule({
     imports: [CommonModule, RouterModule.forRoot(ProductRoute), PanierModule],
-    declarations: [SimpleviewComponent, DetailedviewComponent, ProductlistComponent],
+    declarations: [SimpleviewComponent, DetailedviewComponent, ProductlistComponent, ProductPipe],
     exports: [SimpleviewComponent, DetailedviewComponent, ProductlistComponent]
 })
 export class ProductModule {
