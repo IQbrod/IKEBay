f5a61d06bb85b2b98ab2918f5cec5a9a900db161
magelion <www.max87@yahoo.fr>
2018-12-17 15:28:57 +0100

    StockSetQuantite

--- src/main/webapp/app/stock/component/detailStock/detailStock.component.ts
+++ src/main/webapp/app/stock/component/detailStock/detailStock.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { StockService } from 'app/stock/service/stock.service';
 import { Stock } from 'app/stock/stock.model';
+import { ProductService } from 'app/product/product.service';
+import { Product } from 'app/product/product.model';
 
 @Component({
     selector: 'jhi-detail-stock',
@@ -8,10 +10,21 @@ import { Stock } from 'app/stock/stock.model';
     styleUrls: ['detailStock.css']
 })
 export class DetailStockComponent implements OnInit {
-    @Input() id: Number;
+    @Input() id: number;
     @Input() stock: Stock;
+    products: Product[] = [];
+    quantite: number;
+    constructor(private stockService: StockService, private productService: ProductService) {}
 
-    constructor(private stockService: StockService) {}
+    ngOnInit() {
+        this.productService.listAll().subscribe((prods: Product[]) => (this.products = prods));
+    }
 
-    ngOnInit() {}
+    onKeydown(event) {
+        if (event.key === 'Enter') {
+            //alert('you just clicked enter');
+            this.quantite = parseInt(event.target.value);
+            this.stockService.setQuantity(this.id, this.quantite);
+        }
+    }
 }
