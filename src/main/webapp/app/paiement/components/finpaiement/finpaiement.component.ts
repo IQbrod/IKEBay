8b4b4d406c9c10b8b83338ddd6f1f46eed7cf85c
IQbrod <iqbrod@outlook.fr>
2018-12-20 14:04:43 +0100

    Fix vider Panier après paiement

--- src/main/webapp/app/paiement/components/finpaiement/finpaiement.component.ts
+++ src/main/webapp/app/paiement/components/finpaiement/finpaiement.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { PaiementInfos } from 'app/paiement/paiementinfo.model';
 import { PaiementService } from 'app/paiement/paiement.service';
+import { PanierService } from 'app/panier/service/panier.service';
 
 @Component({
     selector: 'jhi-finpaiement',
@@ -10,10 +11,11 @@ import { PaiementService } from 'app/paiement/paiement.service';
 export class FinPaiementComponent implements OnInit {
     @Input() form: PaiementInfos;
 
-    constructor(private paiementserv: PaiementService) {}
+    constructor(private paiementserv: PaiementService, private panierService: PanierService) {}
 
     ngOnInit() {
         this.form = this.paiementserv.formobject;
         this.form.name = this.form.name.toUpperCase();
+        this.panierService.ViderPanier();
     }
 }
